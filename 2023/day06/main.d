916049677e987ebc345535b9d2536bb393eb8810
Radoslaw Rusiniak <rarusini@microsoft.com>
2023-12-06 21:13:23 +0100

    aoc 2023 day 6 part 1

---
+++ 2023/day06/main.d
@@ -0,0 +1,62 @@
+import core.bitop, std.bitmanip;
+import core.checkedint;
+import core.time;
+import std.algorithm, std.functional, std.meta;
+import std.array, std.container;
+import std.bigint;
+import std.conv;
+import std.format;
+import std.math, std.numeric;
+import std.range, std.range.interfaces;
+import std.stdio, std.string;
+import std.ascii, std.typecons;
+import std.datetime.date, std.datetime.systime;
+
+alias ResultType = int;
+
+alias Times = int[];
+alias Distances = int[];
+alias Race = Tuple!(int, "time", int, "distance");
+alias State = Tuple!(Times, "times", Distances, "distances");
+
+void main(string[] args) {
+    string[] input;
+    string line;
+    while ((line = readln.strip) !is null) { input ~= line; }
+
+    auto state = input.parse;
+
+    debug { state.writeln; }
+
+    auto res = solveEasy(state);
+    // auto res = solveHard(state);
+
+    res.writeln;
+}
+
+int[] parseArrayLine(string input) => input.split(":")[1].split.map!(to!int).array;
+
+Times parseTimes(string input) => input.parseArrayLine;
+
+Distances parseDistances(string input) => input.parseArrayLine;
+
+State parse(string[] input) {
+    auto ts = input[0].parseTimes;
+    auto ds = input[1].parseDistances;
+    return State(ts, ds);
+}
+
+ResultType numberOfWaysToBeat(Race race) 
+    =>
+    iota(1, race.time)
+    .count!(speed => speed * (race.time - speed) > race.distance);
+
+ResultType solveEasy(State state)
+    =>
+    zip(state.times, state.distances)
+    .map!(pipe!(Race, numberOfWaysToBeat))
+    .fold!((a, b) => a * b);
+
+ResultType solveHard(State state) {
+    return 0;
+}
