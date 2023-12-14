58845a070341ff7f8e9652e1dde9f537565129e7
Radoslaw Rusiniak <rarusini@microsoft.com>
2023-12-14 03:24:44 +0100

    aoc 2023 day 11 part 2

--- 2023/day11/main.d
+++ 2023/day11/main.d
@@ -17,8 +17,8 @@ void main(string[] args) {
     string line;
     while ((line = readln.strip) !is null) { input ~= line; }
 
-    auto res = solveEasy(input);
-    // auto res = solveHard(input);
+    // auto res = solveEasy(input);
+    auto res = solveHard(input);
 
     res.writeln;
 }
@@ -40,6 +40,17 @@ string[] expandColumns(string[] input) => input.transpose.expandRows.transpose;
 
 string[] expand(string[] input) => input.expandRows.expandColumns;
 
+long getTotalDistances(Tuple!(int, int)[] pts) {
+    long tot = 0;
+    foreach (i, p1; pts) {
+        foreach (p2; pts[i+1 .. $]) {
+            auto cur = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
+            tot += cur;
+        }
+    }
+    return tot;
+}
+
 long solve(string[] input) {
     Tuple!(int, int)[] pts;
     foreach (i, rw; input) {
@@ -48,17 +59,38 @@ long solve(string[] input) {
         }
     }
     
-    long tot = 0;
-    foreach (i, p1; pts) {
-        foreach (p2; pts[i+1 .. $]) {
-            auto cur = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
-            tot += cur;
-        }
-    }
-
-    return tot;
+    return pts.getTotalDistances;
 }
 
 long solveEasy(string[] input) => input.expand.solve;
 
-int solveHard(string[] input) => 0;
+int[] getEmptyIndices(string[] input) {
+    int[] res;
+    foreach (i, rw; input) { if (rw.all!(c => c == '.')) { res ~= i; } }
+    return res;
+}
+
+long solveHard(string[] input) {
+    auto emptyRows = input.getEmptyIndices;
+    input = input.transpose;
+    auto emptyCols = input.getEmptyIndices;
+    input = input.transpose;
+
+    Tuple!(int, int)[] pts;
+    auto emptyRwIdx = 0;
+    auto emptyColIdx = 0;
+    immutable expand_const = 999_999;
+    foreach (i, rw; input) {
+        if (emptyRwIdx < emptyRows.length && i == emptyRows[emptyRwIdx]) { ++emptyRwIdx; }
+        emptyColIdx = 0;
+        foreach (j, e; rw) {
+            if (emptyColIdx < emptyCols.length && j == emptyCols[emptyColIdx]) { ++emptyColIdx; }
+
+            if (e == '#') {
+                pts ~= tuple(i.to!int + emptyRwIdx * expand_const, j.to!int + emptyColIdx * expand_const);
+            }
+        }
+    }
+
+    return pts.getTotalDistances;
+}
