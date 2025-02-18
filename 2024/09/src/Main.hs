d8d6d17e450853edd3897178a984370d0bd1c4c0
Radoslaw Rusiniak <rarusini@microsoft.com>
2025-02-19 00:29:03 +0100

    Day 9 part 2

--- 2024/09/src/Main.hs
+++ 2024/09/src/Main.hs
@@ -1,9 +1,6 @@
-import System.IO  
 import System.Environment (getArgs)
-import Control.Monad
 import Data.List as L
 import Data.Char
-import Data.Foldable
 import Data.Maybe
 
 main = do
@@ -27,51 +24,67 @@ solveHard = getResultHard. parseHard
 parseEasy :: String -> Input
 parseEasy = toDisk. map digitToInt. takeWhile isDigit
 
+parseHard :: String -> Input
+parseHard = parseEasy
+
 toDisk :: [Int] -> [Block]
 toDisk = map (uncurry Block). zip indexation
     where
     indexation = intersperse Nothing. map Just $ [0..]
 
 getResultEasy :: Input -> Int
-getResultEasy = getScore. refragmentDisk
+getResultEasy = getScore. refragmentDisk greedyInsert
 
-refragmentDisk :: Disk -> Disk
-refragmentDisk disk = foldr refragmentFile disk (files disk)
+getResultHard :: Input -> Int
+getResultHard = getScore. refragmentDisk wholeInsert
+
+refragmentDisk :: InsertAlgo -> Disk -> Disk
+refragmentDisk algo disk = foldr refragmentFile disk (files disk)
     where
     refragmentFile file = 
-        concat. snd. mapAccumL greedyInsert (Just file). removeFile file
+        concat. snd. mapAccumL (insertFile algo) (Just file). removeFile file
     removeFile f@(Block _ l) (x:xs)
         | f == x    = (Block Nothing l) : xs
         | otherwise = x : removeFile f xs
 
-type BlockToInsert = Maybe Block
-type ComparedBlock = Block
 type LeftToInsert = Maybe Block
 type ResultingBlocks = [Block]
-greedyInsert :: BlockToInsert -> ComparedBlock -> (LeftToInsert, ResultingBlocks)
-greedyInsert Nothing b = (Nothing, [b])
-greedyInsert toInsert b@(Block (Just _) _) = (toInsert, [b])
-greedyInsert (Just (Block (Just idx) need)) (Block _ space) =
-    (toInsertBlock, insertedBlock : leftover)
+type Result = (LeftToInsert, ResultingBlocks)
+type InsertAlgo = Index -> Need -> Space -> Result
+type BlockToInsert = Maybe Block
+type ComparedBlock = Block
+insertFile :: InsertAlgo -> BlockToInsert -> ComparedBlock -> Result
+insertFile _ Nothing b = (Nothing, [b])
+insertFile _ toInsert b@(Block (Just _) _) = (toInsert, [b])
+insertFile algo (Just (Block (Just idx) need)) (Block _ space) = 
+    algo idx need space
+
+type Need = Int
+type Space = Int
+greedyInsert :: InsertAlgo
+greedyInsert idx need space = (toInsertBlock, insertedBlock : leftover)
+    where
+    insertedLen = min space need
+    leftSpace = space - insertedLen
+    toInsertBlock = if insertedLen < need
+        then Just (Block (Just idx) (need - insertedLen))
+        else Nothing
+    insertedBlock = Block (Just idx) insertedLen 
+    leftover = if leftSpace > 0
+        then [Block Nothing leftSpace]
+        else []
+
+wholeInsert :: InsertAlgo
+wholeInsert idx need space
+    | need > space = (Just (Block (Just idx) need), [Block Nothing space])
+    | otherwise = (Nothing, (Block (Just idx) need) : leftover)
         where
-        insertedLen = min space need
-        leftSpace = space - insertedLen
-        toInsertBlock = if insertedLen < need
-            then Just (Block (Just idx) (need - insertedLen))
-            else Nothing
-        insertedBlock = Block (Just idx) insertedLen 
-        leftover = if leftSpace > 0
-            then [Block Nothing leftSpace]
+        leftover = if need < space
+            then [Block Nothing (space - need)]
             else []
 
-parseHard :: String -> Input
-parseHard = parseEasy
-
-getResultHard :: Input -> Int
-getResultHard = const 0
-
 getScore :: Disk -> Int
-getScore = fst. foldl go (0, 0)
+getScore = fst. foldl' go (0, 0)
     where
     go (score, pos) (Block idx len) = (score + calculateForBlock, nextPos)
         where 
