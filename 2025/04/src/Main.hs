554647355a004405adb37fb8adb2ec9a2d2774bc
Radoslaw Rusiniak <Radoslaw.Rusiniak@gmail.com>
2025-12-04 14:41:52 +0100

    day 4, part 1

---
+++ 2025/04/src/Main.hs
@@ -0,0 +1,40 @@
+import System.Environment (getArgs)
+import Data.Char (isSpace)
+import Data.Array qualified as Array
+import Linear.V2 (V2(..))
+
+main :: IO ()
+main = do
+  file <- readFile . head =<< getArgs
+  print . part1 $ file
+  print . part2 $ file
+
+type Input = String
+type Parsed = Array.Array (V2 Int) Char
+
+part1, part2 :: Input -> Int
+part1 = getResultPart1 . parse
+part2 = getResultPart2 . parse
+
+parse :: Input -> Parsed
+parse input = Array.listArray (V2 1 1, V2 rows columns) $ filter (not . isSpace) input
+  where
+    rows = length lined
+    columns = length $ head lined
+    lined = lines input
+
+getResultPart1 :: Parsed -> Int
+getResultPart1 arr = length [ () | ix <- Array.indices arr, isPaper ix, reachable ix ]
+  where
+    isPaper ix = arr Array.! ix == '@'
+    reachable ix = (< 4) . length . filter (== '@') $ neighbours ix arr
+    
+neighbours :: V2 Int -> Parsed -> [Char]
+neighbours start@(V2 rw cl) arr = [ arr Array.! adj |
+      x <- [rw-1 .. rw+1], y <- [cl-1 .. cl+1],
+      let adj = V2 x y,
+      start /= adj,
+      Array.inRange (Array.bounds arr) adj]
+
+getResultPart2 :: Parsed -> Int
+getResultPart2 = const 0
