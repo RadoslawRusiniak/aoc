4b3fa7a0cc3028de9956491ae8b179c9f417017d
Radoslaw Rusiniak <Radoslaw.Rusiniak@gmail.com>
2025-12-05 00:35:37 +0100

    day 2, part 2

--- 2025/02/src/Main.hs
+++ 2025/02/src/Main.hs
@@ -1,5 +1,6 @@
 import System.Environment (getArgs)
 import Data.List.Extra (splitOn)
+import Data.Set qualified as Set (fromList, toList)
 
 main :: IO ()
 main = do
@@ -26,18 +27,30 @@ parseInterval s = (fstInt, sndInt)
     sndInt = read . tail . dropWhile (/= '-') $ s
 
 getResultPart1 :: Parsed -> Int
-getResultPart1 intervals = sum . filter (inAnyInterval intervals) . takeWhile (<= mxBound) . map doubleUp $ [1 ..]
-  where
-    mxBound = maximum $ map snd intervals
-
-doubleUp :: Int -> Int
-doubleUp = read . (\s -> s ++ s) . show
+getResultPart1 intervals = sum . getInvalidIds intervals . map (repeatInt 2) $ [1 ..]
 
 inAnyInterval :: [Interval] -> Int -> Bool
 inAnyInterval intervals x = any (inInterval x) intervals
+
+getInvalidIds :: [Interval] -> [Int] -> [Int]
+getInvalidIds intervals = filter (inAnyInterval intervals) . takeWhile (<= mxBound)
+  where
+    mxBound = maximum $ map snd intervals
 
 inInterval :: Int -> Interval -> Bool
 inInterval x (left, right) = left <= x && x <= right
 
 getResultPart2 :: Parsed -> Int
-getResultPart2 = const 0
+getResultPart2 intervals = sum . mergeElements . map (getInvalidIds intervals . getNumbersForRepetition) $ [2 .. 11]
+
+mergeElements :: [[Int]] -> [Int]
+mergeElements = Set.toList . mconcat . map Set.fromList
+
+getNumbersForRepetition :: Int -> [Int]
+getNumbersForRepetition n = map (repeatInt n) [1 ..]
+
+repeatInt :: Int -> Int -> Int
+repeatInt n = read . repeatString n . show
+
+repeatString :: Int -> String -> String
+repeatString n = mconcat . replicate n
