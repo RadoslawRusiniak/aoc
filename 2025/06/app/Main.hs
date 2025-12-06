524205d1ff9323bf33550b5bda6621e0709c17ef
Radoslaw Rusiniak <Radoslaw.Rusiniak@gmail.com>
2025-12-06 06:50:47 +0100

    day 6, part 2

--- 2025/06/app/Main.hs
+++ 2025/06/app/Main.hs
@@ -1,5 +1,7 @@
 import System.Environment (getArgs)
+import Data.Char (isSpace)
 import Data.List (transpose)
+import Data.List.Split (splitWhen)
 
 main :: IO ()
 main = do
@@ -13,14 +15,14 @@ type Parsed = (NumbersMatrix, Operations)
 type NumbersMatrix = [[Int]]
 type Numbers = [Int]
 type Operations = [Operation]
-data Operation = Mul | Add
+data Operation = Mul | Add deriving (Show)
 
 part1, part2 :: Input -> Int
-part1 = getResultPart1 . parse
-part2 = getResultPart2 . parse
+part1 = getResultPart1 . parsePart1
+part2 = getResultPart2 . parsePart2
 
-parse :: Input -> Parsed
-parse = (\xs -> (parseNumbers (init xs), parseOperations (last xs))) . lines
+parsePart1 :: Input -> Parsed
+parsePart1 = (\xs -> (parseNumbers (init xs), parseOperations (last xs))) . lines
 
 parseNumbers :: [String] -> [[Int]]
 parseNumbers = transpose . map parseLine
@@ -30,6 +32,23 @@ parseNumbers = transpose . map parseLine
 parseOperations :: String -> Operations
 parseOperations = map (\w -> if w == "+" then Add else Mul) . words
 
+parsePart2 :: Input -> Parsed
+parsePart2 input = (numbers input, ops input)
+  where
+    numbers = parseVertNumbers . init . lines
+    ops = parseOperations . last . lines
+
+parseVertNumbers :: [String] -> NumbersMatrix
+parseVertNumbers = map readDigitNumbers . splitWhen isBlankLine . init . transpose
+  where
+    isBlankLine = all isSpace
+    readDigitNumbers = map readDigitNumber
+    readDigitNumber = read . trim
+
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
+  where dropWhileEnd f = reverse . dropWhile f . reverse
+
 getResultPart1 :: Parsed -> Int
 getResultPart1 (numbersMatrix, operations) = sum . map (uncurry applyOp) $ zip operations numbersMatrix
 
@@ -38,4 +57,4 @@ applyOp Add = foldl1 (+)
 applyOp Mul = foldl1 (*)
 
 getResultPart2 :: Parsed -> Int
-getResultPart2 = const 0
+getResultPart2 = getResultPart1
